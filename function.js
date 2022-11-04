c4d16592fc119d5509de79d5dc557f3e2c7fddb9
Lubna Tahreem <lubnatahreem789@gmail.com>
2022-11-04 16:23:05 +0530

    created function file

---
+++ function.js
@@ -0,0 +1,14 @@
+function mul(x){
+    return x*5;
+}
+
+const arrr=[];
+const arr1=[1,2,3,4,5,6,7,8,9,10];
+
+for(let i=0;i<arr1.length;i++){
+
+    arrr.push(mul(arr1[i]));
+ 
+}
+console.log(arrr);
+console.log("hi");
